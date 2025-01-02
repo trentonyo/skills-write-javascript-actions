4d416dd894dfb94763aa366ad13151b3e572adb4
Trenton Young <trentonyo@gmail.com>
2025-01-01 19:09:37 -0800

    Support for multiple jokes

--- .github/actions/joke-action/main.js
+++ .github/actions/joke-action/main.js
@@ -2,9 +2,16 @@ const getJoke = require("./joke");
 const core = require("@actions/core");
 
 async function run() {
-  const joke = await getJoke();
-  console.log(joke);
-  core.setOutput("joke-output", joke);
+  const numJokes = parseInt(process.env.INPUT_NUMBER_OF_JOKES, 10) || 1
+  let jokes = []
+  
+  for (let i = 0; i < numJokes; i++) {
+    const joke = await getJoke();
+    jokes.push(joke);
+    console.log(joke);
+  }
+  
+  core.setOutput("joke-output", jokes);
 }
 
 run();
